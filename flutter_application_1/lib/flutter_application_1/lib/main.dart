b9ecf816ac6a2c0f9aa8d19ffd40b61b5b72b96a
Cloe <you@example.com>
2021-10-20 09:55:19 -0400

    2

--- flutter_application_1/lib/flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/flutter_application_1/lib/main.dart
@@ -1,47 +1,32 @@
-import 'package:flutter/material.dart';  
+import 'package:flutter/material.dart';
 
-  
+void main() => runApp(const MyApp());
 
-void main() => runApp(const MyApp());  
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-  
-
-  
-
-class MyApp extends StatelessWidget {  
-
-  
-
-  const MyApp({Key? key}) : super(key: key);  
-
-  
-
-  
-
-  @override  
-
-  Widget build(BuildContext context) { 
-
-    return MaterialApp( 
-
-      title: 'Your app name', 
-
-      home: Scaffold( 
-
-        appBar: AppBar( 
-
-          title: const Text('Your Title here'), 
-
-        ), 
-
-        body: Center>Column[Container,Expanded>Container,Text], 
-
-        ), 
-
-      ), 
-
-    ); 
-
-  } 
-
-} 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Your app name',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Your Title here'),
+        ),
+        body: Center(
+          child: ListView(
+            children: <Widget>[
+              SizedBox(
+                  child:
+                      Container(height: 100, color: const Color(0x00ffffff))),
+              SizedBox(
+                  child:
+                      Container(height: 200, color: const Color(0x00ffffff))),
+              const Text('test'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
